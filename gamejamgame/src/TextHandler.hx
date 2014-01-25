3d1583fa7a42c9c9c5efe1808fd2e6ba4b624f65
ZacharyWa <zcw09@live.com>
2014-01-25 15:55:25 -0600

    Text adding and removing
    Press 'W' so see what I am doing to remove and display more text.

--- gamejamgame/src/TextHandler.hx
+++ gamejamgame/src/TextHandler.hx
@@ -12,34 +12,40 @@ import flash.text.TextFormat;
 
 class TextHandler extends Main
 {
-	public var textTest:Array<TextField>; //Array of strings, each string should only be a word for the sentence.
-	//public var textFormat:TextFormat;
-	//text = "This is a story about your life!";
+	public var textFull:Array<TextField>; //Array of strings, each string should only be a word for the sentence.
+	public var textCurrent:Array<TextField>;
+	public var testSwitch:Bool = true;
+	private var textPosition:Int = 0;
+	
 	public function new() 
 	{
 		super();
-		textTest = new Array<TextField>();
+		textFull = new Array<TextField>();
 		var textFormat = new TextFormat();
 		textFormat.font = "Warnock";
 		textFormat.color = 0x000000;
-		textFormat.size = 18;
+		textFormat.size = 16;
 		var text = new TextField();
 		text.text = "This is a story about your life!";
 		text.x = 20;
 		text.y = Lib.current.stage.stageHeight - 40;
 		text.width = 290;
 		text.setTextFormat(textFormat);
-		textTest.push(text);
+		textFull.push(text);
 		
 
-		Lib.current.stage.addChild(textTest[0]);
+		Lib.current.stage.addChild(textFull[0]);
 	}
 	
-	public function removeTextTest()
+	public function removeText()
 	{
-			Lib.current.stage.removeChild(textTest[0]);
+		Lib.current.stage.removeChild(textFull[0]);
+		textFull.pop();
 	}
 	
-	
-	
+	public function displayText(inputText:TextField)
+	{
+		textFull.push(inputText);
+		Lib.current.stage.addChild(inputText);
+	}
 }
